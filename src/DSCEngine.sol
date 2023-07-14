699edb3a880acf08528e6893b1e48220b479b4d8
Ritik-Agarwal50 <ritikagarwal570@gmail.com>
2023-07-14 12:11:47 +0530

    Corrected Function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -303,7 +303,7 @@ contract DSCEngine is ReentrancyGuard {
 
     function _getUsdValue(address token, uint256 amount) private view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
-        (, int256 price,,,) = priceFeed.staleCheckLatestRoundData();
+        (, int256 price,,,) = priceFeed.latestRoundData();
         // 1 ETH = 1000 USD
         // The returned value from Chainlink will be 1000 * 1e8
         // Most USD pairs have 8 decimals, so we will just pretend they all do

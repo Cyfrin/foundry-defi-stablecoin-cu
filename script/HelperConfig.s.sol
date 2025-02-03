069334c277da4b6a5f4169fda37e480f98a033da
0xByteKnight <0xbyteknight@gmail.com>
2025-02-03 04:56:01 +0100

    Change order of parameters in the NetworkConfig returned in the getOrCreateAnvilEthConfig function (#110)

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -56,8 +56,8 @@ contract HelperConfig is Script {
 
         anvilNetworkConfig = NetworkConfig({
             wethUsdPriceFeed: address(ethUsdPriceFeed), // ETH / USD
-            weth: address(wethMock),
             wbtcUsdPriceFeed: address(btcUsdPriceFeed),
+            weth: address(wethMock),
             wbtc: address(wbtcMock),
             deployerKey: DEFAULT_ANVIL_PRIVATE_KEY
         });
